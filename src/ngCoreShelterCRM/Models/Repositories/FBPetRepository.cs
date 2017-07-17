c48b421a1a63363290b8422acf54123982328e6d
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-07-17 15:33:26 -0700

    Implement data access layer

--- src/ngCoreShelterCRM/Models/Repositories/FBPetRepository.cs
+++ src/ngCoreShelterCRM/Models/Repositories/FBPetRepository.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ngCoreShelterCRM.Services.Pets;
 
 namespace ngCoreShelterCRM.Models.Repositories
 {
@@ -13,19 +14,19 @@ namespace ngCoreShelterCRM.Models.Repositories
     {
         protected static FirebaseClient db = new FirebaseClient("https://usingwithcsharp.firebaseio.com/");
 
+        protected static PetDataAccess dataAccess = new PetDataAccess();
+
         /// <summary>
-        /// Trying design patterns and failing w/headache, gets list of pets from firebase
+        /// Gets all pets from database
         /// </summary>
-        /// <returns>List of Pet Objects</returns>
+        /// <returns>Task List of Pet Objects</returns>
         public async Task<List<Pet>> Pets()
         {
-            var pets = await db
-              .Child("pets")
-              .OrderByKey()
-              .OnceAsync<Pet>();
+            var pets = await dataAccess.Pets();
 
             List<Pet> petList = new List<Pet>();
 
+            //needs transform layer
             foreach (var pet in pets)
             {
                 Pet currentPet = new Pet();
@@ -37,28 +38,34 @@ namespace ngCoreShelterCRM.Models.Repositories
             return petList;
         }
 
+        /// <summary>
+        /// Gets single pet from database using id/key
+        /// </summary>
+        /// <param name="id">ID/Key of pet</param>
+        /// <returns>Task/single pet object</returns>
         public async Task<Pet> GetPet(string id)
         {
-            var pet = await db
-                .Child("pets")
-                .Child(id)
-                .OnceSingleAsync<Pet>();
+            var pet = await dataAccess.GetPet(id);
 
             pet.Key = id;
 
             return pet;
         }
 
+        /// <summary>
+        /// Add single pet to databases
+        /// </summary>
+        /// <param name="data">ID/Key of pet</param>
+        /// <returns>Pet added to db with key</returns>
         public async Task<Pet> AddPet(string data)
         {
             //this should call a transformation service
             Pet newPet = JsonConvert.DeserializeObject<Pet>(data);
-                      
-            var pet = await db
-              .Child("pets")
-              .PostAsync(newPet);
+          
+            newPet.Key = await dataAccess.AddPet(newPet);
 
             return newPet;
+            //This method will eventually return a success/fail code
         }
     }
 }
---
+++ src/ngCoreShelterCRM/Services/Pets/PetDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Firebase.Database;
+using ngCoreShelterCRM.Models;
+using Firebase.Database.Query;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ngCoreShelterCRM.Services.Pets
+{
+    public class PetDataAccess
+    {
+        protected static FirebaseClient db = new FirebaseClient("https://usingwithcsharp.firebaseio.com/");
+
+        public async Task<List<FirebaseObject<Pet>>> Pets()
+        {
+            var pets = await db
+              .Child("pets")
+              .OrderByKey()
+              .OnceAsync<Pet>();
+            return pets.ToList();
+        }
+
+        public async Task<Pet> GetPet(string id)
+        {
+            var pet = await db
+                .Child("pets")
+                .Child(id)
+                .OnceSingleAsync<Pet>();
+
+            return pet;
+        }
+
+        public async Task<string> AddPet(Pet pet)
+        {
+            //should return success/fail, currently returns key
+            var newPet = await db
+                .Child("pets")
+                .PostAsync(pet);
+
+            return newPet.Key;
+        }
+    }
+}
