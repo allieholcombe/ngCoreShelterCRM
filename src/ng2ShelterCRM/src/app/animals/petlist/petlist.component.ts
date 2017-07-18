b353f7a9484a1b4b212f6d079a3dfbe6d7ebce14
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-07-18 16:08:39 -0700

    Add logic to switch between multiple and single results

--- src/ng2ShelterCRM/src/app/animals/petlist/petlist.component.ts
+++ src/ng2ShelterCRM/src/app/animals/petlist/petlist.component.ts
@@ -10,24 +10,31 @@ import { PetsRepository } from './../../services/pets/pets.repository.service';
 export class PetListComponent implements OnInit {
   result: any;
   isComplete: boolean = false;
+  isMultiple: boolean = false;
 
   constructor(private _repo: PetsRepository) { }
 
   ngOnInit() {
   }
 
+//NEXT STEP: FORM VALIDATION
   //json returns this with extra array, not sure why
   retrievePets() {
     this.isComplete = false;
     this._repo.getAllPets()
       .subscribe((data: Response) => this.result = data,
                  (error: any) => console.log("You borked it"),
-                 () => this.isComplete = true);
+                 () => {this.isComplete = true;
+                        this.isMultiple = true;});
   }
 
   retrieveSinglePet(id: string) {
-    this._repo.getSinglePet(id);
-    console.log(this.result);
+    this.isMultiple = false;
+    this.isComplete = false;
+    this._repo.getSinglePet(id)
+      .subscribe((data: Response) => this.result = data,
+                 (error: any) => console.log("You borked it"),
+                 () => this.isComplete = true);
   }
 
 }
