ef94ea468f19c6899ec9b500960ade70fdcdec04
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-07-26 14:44:24 -0700

    Rearrange global vars, implement tolerable navbar

--- src/ng2ShelterCRM/src/app/_layouts/secure/header/header.component.ts
+++ src/ng2ShelterCRM/src/app/_layouts/secure/header/header.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from './../../../_services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [AuthService]
 })
 export class HeaderComponent implements OnInit {
   public isCollapsed;
 
-  constructor() { }
+  constructor(private _auth: AuthService) { }
 
   ngOnInit() {
   }
 
-  public get menuIcon(): string {
-    return this.isCollapsed ? '☰' : '✖';
+  logOut() {
+    this._auth.logOut();
   }
+
 }
